9cdf615023efc445d115aabbfc24bcfbfc6957f3
Divakar Raut <thewalker@Divakars-MacBook-Pro.local>
2021-03-17 22:37:39 +1100

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {Button, Container, Row, Col }from 'react-bootstrap';
+import { Container, Row, Col }from 'react-bootstrap';
 import './App.css';
 import {AddForm} from './components/form/Addform';
 import {TaskLists} from './components/taskList/TaskLists';

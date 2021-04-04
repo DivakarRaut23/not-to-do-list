5d6c41637ab461cf35d5d9bf9d5021a46f172da3
Divakar Raut <divakar.raut@service.nsw.gov.au>
2021-04-04 14:38:53 +1000

    production changes

--- api-not-to-do-app/config/db.js
+++ api-not-to-do-app/config/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 
 const mongoClient = async () => {
 
-    const connStr = process.env.NODE_ENV !== "production" ? process.env.PROD_MONGO_CLIENT : process.env.MONGO_CLIENT
+    const connStr = process.env.NODE_ENV === "production" ? process.env.PROD_MONGO_CLIENT : process.env.MONGO_CLIENT
 
     try {
    const con = await mongoose.connect(connStr, {
